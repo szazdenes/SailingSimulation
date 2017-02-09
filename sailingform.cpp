d26d35c0a6f0103145d35e3b699d6bfe4b95bd29
szazdenes <szaz.denes@gmail.com>
2017-02-09 21:56:20 +0100

    coordinate map added corrections

--- sailingform.cpp
+++ sailingform.cpp
@@ -245,7 +245,7 @@ void SailingForm::drawUnitVectors(QImage &image, QGraphicsScene &scene, QColor &
     painter.drawPoint(shift.x(), shift.y());
     pen.setColor(Qt::gray);
     painter.setPen(pen);
-    painter.drawPoint(165.0, shift.y());
+    painter.drawPoint((image.width()/86.98)*(-42.7 - (-67.989)), shift.y());
 
     painter.end();
     scene.clear();
@@ -273,7 +273,7 @@ void SailingForm::drawNavigationEndPoint(QImage &image, QGraphicsScene &scene, Q
     painter.drawPoint(shift.x(), shift.y());
     pen.setColor(Qt::gray);
     painter.setPen(pen);
-    painter.drawPoint(165.0, shift.y());
+    painter.drawPoint((image.width()/86.98)*(-42.7 - (-67.989)), shift.y());
 
     painter.end();
     scene.addPixmap(QPixmap::fromImage(image));
@@ -345,7 +345,7 @@ void SailingForm::on_startPushButton_clicked()
     fitImage(background, ui->trajectoryGraphicsView);
     fitImage(background, ui->multipleRunGraphicsView);
 
-    double lengthOfVectorList = qAbs(trajectoryImage.width()-(1-601.0/trajectoryImage.width())*trajectoryImage.width() - 165.0);
+    double lengthOfVectorList = qAbs((trajectoryImage.width()/86.98)*(5.3 - (-67.989)) - (trajectoryImage.width()/86.98)*(-42.7 - (-67.989)));
     double voyageTime = distance/ui->speedDoubleSpinBox->value();
 
     for (int z = 1; z <= 3; z++){
@@ -368,7 +368,6 @@ void SailingForm::on_startPushButton_clicked()
             if(ui->solRadioButton->isChecked()){
                 startingTime = 3;
                 lengthOfDay = 17;
-//                ui->speedDoubleSpinBox->setValue(distance/(ui->simLengthSpinBox->value()*lengthOfDay));
             }
             if(ui->equRadioButton->isChecked()){
                 startingTime = 6;
@@ -392,11 +391,11 @@ void SailingForm::on_startPushButton_clicked()
                 }
             }
             if(!unitStepVectorList.isEmpty())
-                drawNavigationEndPoint(endPointImage, scene2, color, unitStepVectorList, QPointF(endPointImage.width()-(1-601.0/endPointImage.width())*endPointImage.width(), endPointImage.height()-(1-332.0/endPointImage.height())*endPointImage.height()));
+                drawNavigationEndPoint(endPointImage, scene2, color, unitStepVectorList, QPointF((endPointImage.width()/86.98)*(5.3 - (-67.989)), -1*(endPointImage.height()/33.59)*(61 - 83.599)));
             QApplication::processEvents();
         }
         if(!unitStepVectorList.isEmpty())
-            drawUnitVectors(trajectoryImage, scene1, color, unitStepVectorList, QPointF(trajectoryImage.width()-(1-601.0/trajectoryImage.width())*trajectoryImage.width(), trajectoryImage.height()-(1-332.0/trajectoryImage.height())*trajectoryImage.height()));
+            drawUnitVectors(trajectoryImage, scene1, color, unitStepVectorList, QPointF((trajectoryImage.width()/86.98)*(5.3 - (-67.989)), -1*(trajectoryImage.height()/33.59)*(61 - 83.599)));
 
     }
 
